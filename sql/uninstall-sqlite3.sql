1ca38146d11bfd7a06184ef294730dfa70532328
Sebastian Freundt <hroptatyr@fresse.org>
2011-03-06 17:48:31 +0000

    minor, provide installation sql for sqlite3
    Signed-off-by: Sebastian Freundt <hroptatyr@fresse.org>

---
+++ sql/uninstall-sqlite3.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS "aou_umpf_last";
+DROP TABLE IF EXISTS "aou_umpf_position";
+DROP TABLE IF EXISTS "aou_umpf_security";
+DROP TABLE IF EXISTS "aou_umpf_tag";
+DROP TABLE IF EXISTS "aou_umpf_portfolio";
