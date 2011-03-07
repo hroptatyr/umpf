cb8eba22dbb49ecd1dc95d567588cb70ad7aec18
Sebastian Freundt <sebastian.freundt@ga-group.nl>
2011-03-07 10:49:09 +0000

    minor, support sqlite and mysql databases in be-sql
    Signed-off-by: Sebastian Freundt <sebastian.freundt@ga-group.nl>

--- srv/be-sql.c
+++ srv/be-sql.c
@@ -38,50 +38,144 @@
 #if defined HAVE_CONFIG_H
 # include "config.h"
 #endif	/* HAVE_CONFIG_H */
-#if defined HAVE_MYSQL
+#if defined WITH_MYSQL
 # include <mysql/mysql.h>
-#endif
+#endif	/* WITH_MYSQL */
+#if defined WITH_SQLITE
+# include <sqlite3.h>
+#endif	/* WITH_SQLITE */
 #include "nifty.h"
 #include "be-sql.h"
 
-#define BE_SQL		"mod/pfd/sql"
+#define BE_SQL		"mod/umpf/sql"
 
-#if defined HAVE_MYSQL
-DEFUN dbconn_t
-be_sql_connect(const char *h, const char *u, const char *pw, const char *sch)
+typedef enum {
+	BE_SQL_UNK,
+	BE_SQL_MYSQL,
+	BE_SQL_SQLITE,
+} be_sql_type_t;
+
+static inline dbconn_t
+be_sql_set_type(void *conn, be_sql_type_t type)
+{
+	return (void*)((long int)conn | type);
+}
+
+static inline be_sql_type_t
+be_sql_get_type(dbconn_t conn)
+{
+	/* we abuse the last 3 bits of the conn pointer */
+	return (be_sql_type_t)((long int)conn & 0x07);
+}
+
+static inline void*
+be_sql_get_conn(dbconn_t conn)
+{
+	/* we abuse the last 3 bits of the conn pointer */
+	return (void*)((long int)conn & ~0x07);
+}
+
+#if defined WITH_MYSQL
+static dbconn_t
+be_mysql_open(const char *h, const char *u, const char *pw, const char *sch)
 {
 	MYSQL *conn;
 	conn = mysql_init(NULL);
 	if (!mysql_real_connect(conn, h, u, pw, sch, 0, NULL, 0)) {
-		PFD_DEBUG(BE_SQL "failed to connect\n");
+		UMPF_DEBUG(BE_SQL "failed to connect\n");
 		mysql_close(conn);
 		return NULL;
 	}
 	return conn;
 }
 
-DEFUN void
-be_sql_close(dbconn_t conn)
+static void
+be_mysql_close(dbconn_t conn)
 {
 	(void)mysql_close(conn);
 	return;
 }
 
-#else  /* !HAVE_MYSQL */
+#else
+/* mysql not support, provide stubs */
+static void
+be_mysql_close(dbconn_t UNUSED(conn))
+{
+	return;
+}
 
-DEFUN dbconn_t
-be_sql_connect(
-	const char *UNUSED(host), const char *UNUSED(user),
+static dbconn_t
+be_mysql_open(
+	const char *UNUSED(h), const char *UNUSED(u),
 	const char *UNUSED(pw), const char *UNUSED(sch))
 {
 	return NULL;
 }
+#endif	/* WITH_MYSQL */
 
-DEFUN void
-be_sql_close(dbconn_t UNUSED(conn))
+#if defined WITH_SQLITE
+
+static void
+be_sqlite_close(dbconn_t conn)
+{
+	sqlite3_close(conn);
+	return;
+}
+
+static dbconn_t
+be_sqlite_open(const char *file)
+{
+	sqlite3 *res;
+	sqlite3_open(file, &res);
+	return res;
+}
+
+#else  /* !WITH_SQLITE */
+/* sqlite not support, provide stubs */
+static void
+be_sqlite_close(dbconn_t UNUSED(conn))
 {
 	return;
 }
-#endif	/* HAVE_MYSQL */
+
+static dbconn_t
+be_sqlite_open(const char *UNUSED(file))
+{
+	return NULL;
+}
+#endif	/* WITH_SQLITE */
+
+/* higher level alibi functions */
+DEFUN dbconn_t
+be_sql_connect(const char *h, const char *u, const char *pw, const char *sch)
+{
+	dbconn_t res;
+	if (h == NULL && u == NULL && pw == NULL && sch != NULL) {
+		void *tmp = be_sqlite_open(sch);
+		res = be_sql_set_type(tmp, BE_SQL_SQLITE);
+	} else {
+		void *tmp = be_mysql_open(h, u, pw, sch);
+		res = be_sql_set_type(tmp, BE_SQL_MYSQL);
+	}
+	return res;
+}
+
+DEFUN void
+be_sql_close(dbconn_t conn)
+{
+	switch (be_sql_get_type(conn)) {
+	case BE_SQL_UNK:
+	default:
+		/* don't know what to do */
+		break;
+	case BE_SQL_MYSQL:
+		be_mysql_close(be_sql_get_conn(conn));
+		break;
+	case BE_SQL_SQLITE:
+		be_sqlite_close(be_sql_get_conn(conn));
+		break;
+	}
+	return;
+}
 
 /* be-sql.c ends here */
