2d0e57b955f7031474a89fe2850ef46022bfe816
Sebastian Freundt <sebastian.freundt@ga-group.nl>
2011-03-15 17:24:11 +0000

    fix, spell TransTyp attribute correctly
    Signed-off-by: Sebastian Freundt <sebastian.freundt@ga-group.nl>

--- lib/print-fixml.c
+++ lib/print-fixml.c
@@ -507,7 +507,7 @@ static void
 print_rgst_instrctns(__ctx_t ctx, umpf_msg_t msg, size_t indent)
 {
 	print_indent(ctx, indent);
-	sputs(ctx, "<RgstInstrctns TrsnTyp=\"0\">\n");
+	sputs(ctx, "<RgstInstrctns TransTyp=\"0\">\n");
 
 	print_rg_dtl(ctx, msg->new_pf.name, msg->new_pf.satellite, indent + 2);
 
@@ -520,7 +520,7 @@ static void
 print_rgst_instrctns_rsp(__ctx_t ctx, umpf_msg_t msg, size_t indent)
 {
 	print_indent(ctx, indent);
-	sputs(ctx, "<RgstInstrctnsRsp TrsnTyp=\"0\"");
+	sputs(ctx, "<RgstInstrctnsRsp TransTyp=\"0\"");
 
 	if (msg->new_pf.name) {
 		sputs(ctx, " RegStat=\"A\" ID=\"");
