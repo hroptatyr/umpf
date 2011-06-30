6c4e172bbc8c6c775797904c90de37145f1d964d
Sebastian Freundt <sebastian.freundt@ga-group.nl>
2011-06-30 20:44:03 +0000

    minor, be more logical about the buffer writer deposition
    Signed-off-by: Sebastian Freundt <sebastian.freundt@ga-group.nl>

--- srv/dso-umpf.c
+++ srv/dso-umpf.c
@@ -377,18 +377,15 @@ handle_data(umpf_conn_t ctx, char *msg, size_t msglen)
 		/* definite success */
 		char *buf = NULL;
 		size_t len;
-		umpf_conn_t wr;
+		umpf_conn_t wr = NULL;
 
 		/* serialise, put results in BUF*/
 		if ((len = interpret_msg(&buf, umsg)) &&
 		    (wr = write_soon(ctx, buf, len, wr_fin_cb))) {
 			UMPF_DEBUG(MOD_PRE ": installing buf wr'er %p\n", buf);
 			put_fd_data(wr, buf);
-			put_fd_data(ctx, wr);
-			return 0;
 		}
-		/* kick original context's data */
-		put_fd_data(ctx, NULL);
+		put_fd_data(ctx, wr);
 		return 0;
 
 	} else if (/* umsg == NULL && */p == NULL) {
