e2dda012a44fead44f845b0012f4d1f71ec54328
Sebastian Freundt <freundt@ga-group.nl>
2011-08-10 13:33:23 +0000

    minor, migrate sql installer scripts to m4 based single-source approach
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

---
+++ sql/sqlite3.m4
@@ -0,0 +1,8 @@
+changequote
+changequote(`[', `]')
+
+define([N], ["]$1["])
+define([ON_CONFLICT], [ON CONFLICT])
+define([POST], [])
+
+dnl sqlite3.m4 ends here
--- sql/uninstall-sqlite3.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS "aou_umpf_last";
-DROP INDEX IF EXISTS "aou_umpf_posgrp_fact_flavour";
-DROP TABLE IF EXISTS "aou_umpf_posgrp_fact";
-DROP TABLE IF EXISTS "aou_umpf_posgrp";
-DROP TABLE IF EXISTS "aou_umpf_dposition";
-DROP TABLE IF EXISTS "aou_umpf_position";
-DROP TABLE IF EXISTS "aou_umpf_security";
-DROP TABLE IF EXISTS "aou_umpf_tag";
-DROP TABLE IF EXISTS "aou_umpf_portfolio";
